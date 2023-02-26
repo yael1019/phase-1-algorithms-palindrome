91ec68de23d4f85fc2b36b22d8b2155d58415f5b
Yael Harosh <yaelharosh01@gmail.com>
2023-02-26 18:19:39 -0500

    pass palindrome test

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // declare a variable rev and set it to the reverse of the string
+  let rev = word.split('').reverse().join('');
+  // if word equals rev return true
+  if(word === rev) return true;
+  // otherwise return false
+  else return false;
 }
 
 /* 
   Add your pseudocode here
 */
+// log to the console the result of calling isPalindrome
+// console.log(isPalindrome('mom'));
+// console.log(isPalindrome('rabit'));
 
 /*
   Add written explanation of your solution here
